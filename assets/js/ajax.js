1660c8e70c90e9e1904b738c87e5f360587c381c
Manjana <manjanaaloka12345@gmail.,com>
2025-11-27 10:19:11 +0530

    asd

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,12 +1,38 @@
-$(document).ready(function() {
-    $("#hi-btn").click(function(e) {
+$(document).ready(function () {
+    // $("#contact-form-id").submit(function (e) {
+    //     e.preventDefault();
+    //     var formData = $(this).serialize();
+    //     $.post("https://thecbiglobal.com/_manjana.php", { mail: true, formData: formData },
+    //         function (data, textStatus) {
+    //             var result = JSON.parse(data);
+    //             alert(result['res']);
+    //         }
+    //     );
+    // });
+
+
+    $('#contact-form-id').submit(function (e) {
         e.preventDefault();
-        // $del = $(this).val();
-        $.post("https://thecbiglobal.com/_manjana.php", { 'delete': true },
-            function(data, textStatus) {
-                var result = JSON.parse(data);
-                alert(result['res']);
+        var formData = new FormData(this);
+        formData.append('sendMail', true);
+        $.ajax({
+            url: 'https://thecbiglobal.com/_manjana.php',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                // var response = JSON.parse(response);
+                swal("Success!", "Your message has been sent successfully.", "success");
+                // if(response['sendMail'] == 'ok') {
+                //     // alert(response['email']);
+                //     console.log(response);
+                $('#contact-form-id')[0].reset();
+                // } else {
+                //     console.log(response);
+                //     // swal("Error!", "There was an error sending your message. Please try again later.", "error");
+                // }
             }
-        );
+        });
     });
 });
